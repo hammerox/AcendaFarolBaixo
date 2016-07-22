37b8e0e2b03174f06c5c7b648fb242580966d6ad
Mauricio Custodio <mauricio_rc2@hotmail.com>
2016-07-22 20:26:49 -0300

    Detect activity

--- app/src/main/java/com/hammerox/android/farolnasrodovias/MainActivity.java
+++ app/src/main/java/com/hammerox/android/farolnasrodovias/MainActivity.java
@@ -1,13 +1,121 @@
 package com.hammerox.android.farolnasrodovias;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.location.DetectedActivity;
+
+import io.nlopez.smartlocation.OnActivityUpdatedListener;
+import io.nlopez.smartlocation.SmartLocation;
+
+public class MainActivity extends AppCompatActivity implements OnActivityUpdatedListener {
+
+    private TextView activityText;
+
+    private static final int LOCATION_PERMISSION_ID = 1001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Bind event clicks
+        Button startLocation = (Button) findViewById(R.id.button_start);
+        startLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Location permission not granted
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_ID);
+                    return;
+                }
+                startLocation();
+            }
+        });
+
+        Button stopLocation = (Button) findViewById(R.id.button_stop);
+        stopLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopLocation();
+            }
+        });
+
+        // bind textviews
+        activityText = (TextView) findViewById(R.id.textview_activity);
+
+        // Keep the screen always on
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        showLast();
+    }
+
+
+    private void showActivity(DetectedActivity detectedActivity) {
+        if (detectedActivity != null) {
+            activityText.setText(
+                    String.format("Activity %s with %d%% confidence",
+                            getNameFromType(detectedActivity),
+                            detectedActivity.getConfidence())
+            );
+        } else {
+            activityText.setText("Null activity");
+        }
+    }
+
+
+    private void startLocation() {
+        SmartLocation smartLocation = new SmartLocation.Builder(this).logging(true).build();
+        smartLocation.activity().start(this);
+    }
+
+
+    private void stopLocation() {
+        SmartLocation.with(this).activity().stop();
+        activityText.setText("Activity Recognition stopped!");
+    }
+
+
+    private void showLast() {
+        DetectedActivity detectedActivity = SmartLocation.with(this).activity().getLastActivity();
+        if (detectedActivity != null) {
+            activityText.setText(
+                    String.format("[From Cache] Activity %s with %d%% confidence",
+                            getNameFromType(detectedActivity),
+                            detectedActivity.getConfidence())
+            );
+        }
+    }
+
+
+    private String getNameFromType(DetectedActivity activityType) {
+        switch (activityType.getType()) {
+            case DetectedActivity.IN_VEHICLE:
+                return "in_vehicle";
+            case DetectedActivity.ON_BICYCLE:
+                return "on_bicycle";
+            case DetectedActivity.ON_FOOT:
+                return "on_foot";
+            case DetectedActivity.STILL:
+                return "still";
+            case DetectedActivity.TILTING:
+                return "tilting";
+            default:
+                return "unknown";
+        }
+    }
+
+
+    @Override
+    public void onActivityUpdated(DetectedActivity detectedActivity) {
+        showActivity(detectedActivity);
     }
 }
