ebca47026c4b8f9cd75bd7b97db75d5fabee3f82
Mauricio Custodio <mauricio_rc2@hotmail.com>
2016-08-11 17:18:56 -0300

    Detector running on background

--- app/src/main/java/com/hammerox/android/acendaofarolbaixo/DetectorService.java
+++ app/src/main/java/com/hammerox/android/acendaofarolbaixo/DetectorService.java
@@ -2,14 +2,46 @@ package com.hammerox.android.acendaofarolbaixo;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.text.format.DateUtils;
+import android.util.Log;
+
+import com.google.android.gms.location.DetectedActivity;
+
+import java.text.SimpleDateFormat;
+
+import io.nlopez.smartlocation.OnActivityUpdatedListener;
+import io.nlopez.smartlocation.SmartLocation;
 
 /**
  * Created by Mauricio on 10-Aug-16.
  */
-public class DetectorService extends Service {
+public class DetectorService extends Service implements OnActivityUpdatedListener {
 
+    private boolean mNotifyUser;
+    private StringBuilder mTextLog;
+    private long mTimeNow;
+    private long mLastTime;
+    private SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+
+    public static final String LOG_TAG = "onActivityUpdated";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Start string builder
+        mTextLog = new StringBuilder();
+        mNotifyUser = true;
+
+        SmartLocation smartLocation = new SmartLocation.Builder(this).logging(true).build();
+        smartLocation.activity().start(this);
+        Log.d(LOG_TAG, "DetectorService RUNNING");
+    }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
@@ -17,9 +49,88 @@ public class DetectorService extends Service {
         return super.onStartCommand(intent, flags, startId);
     }
 
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
         return null;
     }
+
+
+    @Override
+    public void onActivityUpdated(DetectedActivity detectedActivity) {
+        if (detectedActivity != null) {
+            showLog(detectedActivity);
+            alarmAlgorithm(detectedActivity);
+        } else {
+//            mTextView.setText("Null activity");
+            Log.d(LOG_TAG, "Null activity");
+        }
+    }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        SmartLocation.with(this).activity().stop();
+        Log.d(LOG_TAG, "DetectorService STOPPED");
+    }
+
+
+    public void playNotification() {
+        try {
+            Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
+            r.play();
+            mNotifyUser = false;
+            Log.d(LOG_TAG, "User notified");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void resetNotificationFlag() {
+        if (!mNotifyUser) {
+            mNotifyUser = true;
+            Log.d(LOG_TAG, "User is now prone to receive notification");
+        }
+    }
+
+
+    public void showLog(DetectedActivity detectedActivity) {
+        long timeDiff = 0L;
+        mTimeNow = System.currentTimeMillis();
+
+        if (mLastTime != 0L) {
+            timeDiff = (mTimeNow - mLastTime) / DateUtils.SECOND_IN_MILLIS;
+        }
+
+        mTextLog.append(sdf.format(mTimeNow))
+                .append(" - ")
+                .append(detectedActivity.toString().substring(23))
+                .append(" - ")
+                .append(timeDiff)
+                .append("\n");
+//        mTextView.setText(mTextLog.toString());
+        Log.d(LOG_TAG, detectedActivity.toString() + " seconds: " + timeDiff);
+
+        mLastTime = mTimeNow;
+    }
+
+
+    public void alarmAlgorithm(DetectedActivity detectedActivity) {
+        int activityType = detectedActivity.getType();
+
+        if (detectedActivity.getConfidence() == 100) {
+            if (activityType == DetectedActivity.IN_VEHICLE) {
+                if (mNotifyUser) playNotification();
+            } else if (activityType == DetectedActivity.STILL || activityType == DetectedActivity.TILTING) {
+                // Do nothing
+            } else {
+                resetNotificationFlag();
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/hammerox/android/acendaofarolbaixo/MainActivity.java
+++ app/src/main/java/com/hammerox/android/acendaofarolbaixo/MainActivity.java
@@ -1,15 +1,14 @@
 package com.hammerox.android.acendaofarolbaixo;
 
 import android.Manifest;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
-import android.net.Uri;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.format.DateUtils;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
@@ -18,22 +17,17 @@ import android.widget.ToggleButton;
 
 import com.google.android.gms.location.DetectedActivity;
 
-import java.text.SimpleDateFormat;
-
-import io.nlopez.smartlocation.OnActivityUpdatedListener;
 import io.nlopez.smartlocation.SmartLocation;
 
-public class MainActivity extends AppCompatActivity implements OnActivityUpdatedListener {
 
-    private boolean mNotifyUser;
-    private StringBuilder mTextLog;
+public class MainActivity extends AppCompatActivity {
+
     private TextView mTextView;
-    private long mTimeNow;
-    private long mLastTime;
-    private SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+    private ToggleButton mToggleButton;
+    private String mServiceName = DetectorService.class.getName();
+    private ActivityManager mActivityManager;
 
     private static final int LOCATION_PERMISSION_ID = 1001;
-    private static final String LOG_TAG = "onActivityUpdated";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,11 +37,18 @@ public class MainActivity extends AppCompatActivity implements OnActivityUpdated
         // Keep the screen always on
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
-        // bind textviews
+        // bind views
         mTextView = (TextView) findViewById(R.id.textview_activity);
+        mToggleButton = (ToggleButton) findViewById(R.id.toggle_detector);
 
-        // Start string builder
-        mTextLog = new StringBuilder();
+        // Find DetectorService running on background
+        // If found, toggle button to ON. If not, toggle to OFF
+        if (findService(mServiceName)) {
+            Log.v(DetectorService.LOG_TAG, "Found Service running");
+            mToggleButton.setChecked(true);
+        } else {
+            mToggleButton.setChecked(false);
+        }
 
         showLast();
     }
@@ -65,99 +66,43 @@ public class MainActivity extends AppCompatActivity implements OnActivityUpdated
     }
 
 
-    @Override
-    public void onActivityUpdated(DetectedActivity detectedActivity) {
-        long timeDiff = 0L;
-        mTimeNow = System.currentTimeMillis();
-
-        if (detectedActivity != null) {
-            if (mLastTime != 0L) {
-                timeDiff = (mTimeNow - mLastTime) / DateUtils.SECOND_IN_MILLIS;
-            }
-
-            mTextLog.append(sdf.format(mTimeNow))
-                    .append(" - ")
-                    .append(detectedActivity.toString().substring(23))
-                    .append(" - ")
-                    .append(timeDiff)
-                    .append("\n");
-            mTextView.setText(mTextLog.toString());
-            Log.d(LOG_TAG, detectedActivity.toString() + " seconds: " + timeDiff);
-
-            mLastTime = mTimeNow;
-
-            int activityType = detectedActivity.getType();
-
-            if (detectedActivity.getConfidence() == 100) {
-                switch (activityType) {
-                    // IN_VEHICLE, play notification
-                    case 0:
-                        if (mNotifyUser) playNotification();
-                        break;
-                    // STILL and TILTING, do nothing
-                    case 3:
-                    case 5:
-                        break;
-                    // Everything else, reset notification flag
-                    default:
-                        resetNotificationFlag();
-                        break;
-                }
-            }
-        } else {
-            mTextView.setText("Null activity");
-            Log.d(LOG_TAG, "Null activity");
-        }
-    }
-
-
-    public void playNotification() {
-        try {
-            Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-            Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
-            r.play();
-            mNotifyUser = false;
-            Log.d(LOG_TAG, "User notified");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-
-    public void resetNotificationFlag() {
-        if (!mNotifyUser) {
-            mNotifyUser = true;
-            Log.d(LOG_TAG, "User is now prone to receive notification");
-        }
-    }
-
-
     public void onToggleClicked(View view) {
         boolean on = ((ToggleButton) view).isChecked();
 
         if (on) {
-            // Enable detector
             // Location permission not granted
             if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_ID);
                 return;
             }
-            startLocation();
+
+            // Enable detector
+            Intent intent = new Intent(this, DetectorService.class);
+            startService(intent);
         } else {
             // Disable detector
-            stopLocation();
+            Intent intent = new Intent(this, DetectorService.class);
+            stopService(intent);
+            mTextView.setText("Activity Recognition stopped!");
         }
     }
 
 
-    private void startLocation() {
-        SmartLocation smartLocation = new SmartLocation.Builder(this).logging(true).build();
-        smartLocation.activity().start(this);
-    }
+    private boolean findService(String serviceName) {
+        if (mActivityManager == null) {
+            mActivityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        }
 
+        // Search
+        for (ActivityManager.RunningServiceInfo service : mActivityManager.getRunningServices(Integer.MAX_VALUE)) {
+            // Check
+            if (serviceName.equals(service.service.getClassName())) {
+                // If found, return true
+                return true;
+            }
+        }
 
-    private void stopLocation() {
-        SmartLocation.with(this).activity().stop();
-        mTextView.setText("Activity Recognition stopped!");
+        // If not found, return false
+        return false;
     }
 }
